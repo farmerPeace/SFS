94324bd6731ec8cd18ada382db0b55e14e2d74db
chris <christianariastorres7@gmail.com>
2023-12-17 23:30:47 +0000

    Creación archivo main

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+void fileMode(string option, string file) {
+  ofstream iFile;
+
+  if (option == "--create" || option == "-c") {
+    iFile.open(file);
+  } else if (option == "--open" || option == "-o") {
+    cout << "Elegiste la opcion 'abrir' para el archivo " << file << endl;
+  } else if (option == "--write" || option == "-w") {
+    cout << "Elegiste la opcion 'abrir' para el archivo " << file << endl;
+  } else if (option == "--remove" || option == "-r") {
+    cout << "Elegiste la opcion 'abrir' para el archivo " << file << endl;
+  } else {
+    cout << "No elegite una opcion valida\n";
+  }
+  exit(0);
+}
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    cout << "Faltan argumentos";
+    exit(-1);
+  }
+  
+  fileMode(argv[1], argv[2]);
+
+  return 0;
+}
